523b8024d13110d44c2504833443cbbda3584a4d
baseballattix <brandonapeck02@gmail.com>
2025-09-29 13:01:51 -0700

    Add LCD header and source files; refactor main.c for state machine implementation

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,212 +1,97 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include <main.h>
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+const struct State {
+uint32_t Out;
+uint32_t Time; // 10 ms units
+uint32_t Next[8]; // list of next states
+};
+typedef const struct State STyp;
+#define goN 0
+#define waitN 1
+#define goE 2
+#define waitE 3
+#define checkN 4
+#define goNtoWalk 5
+#define checkE 6
+#define goEtoWalk 7
+#define preWalk 8
+#define goWalk 9
+#define hurryWalk1 10
+#define hurryWalk2 11
+#define hurryWalk3 12
+#define hurryWalk4 13
+#define stopWalk 14
+STyp FSM[15] = {
+{0x85,1000,{goN,checkN,waitN,checkN,goN,checkN,waitN,checkN}},
+{0x89, 250,{goE,goE,goE,goE,goE,goE,goE,goE}},
+{0x31,1000,{goE,checkE,goE,checkE,waitE,checkE,waitE,checkE}},
+{0x51, 250,{goN,goN,goN,goN,goN,goN,goN,goN}},
+{0x85, 2000, {goN,goNtoWalk,goN,goNtoWalk,goN,goNtoWalk,goN,goNtoWalk}}, 
+{0x89, 250, {preWalk,preWalk,preWalk,preWalk,preWalk,preWalk,preWalk,preWalk}},
+{0x31, 2000, {goE,goEtoWalk,goE,goEtoWalk,goE,goEtoWalk,goE,goEtoWalk}}, 
+{0x51, 250, {preWalk,preWalk,preWalk,preWalk,preWalk,preWalk,preWalk,preWalk}},
+{0x91, 500, {goWalk,goWalk,goWalk,goWalk,goWalk,goWalk,goWalk,goWalk}},
+{0x92, 1000, {hurryWalk1,hurryWalk1,hurryWalk1,hurryWalk1,hurryWalk1,hurryWalk1,hurryWalk1,hurryWalk1}},
+{0x91, 250, {hurryWalk2,hurryWalk2,hurryWalk2,hurryWalk2,hurryWalk2,hurryWalk2,hurryWalk2,hurryWalk2}},
+{0x90, 250, {hurryWalk3,hurryWalk3,hurryWalk3,hurryWalk3,hurryWalk3,hurryWalk3,hurryWalk3,hurryWalk3}},
+{0x91,250, {hurryWalk4,hurryWalk4,hurryWalk4,hurryWalk4,hurryWalk4,hurryWalk4,hurryWalk4,hurryWalk4}},
+{0x90,250, {stopWalk,stopWalk,stopWalk,stopWalk,stopWalk,stopWalk,stopWalk,stopWalk}},
+{0x91,1000, {goN,goWalk,goE,goWalk,goN,goWalk,goN,goWalk}},
+};
 
-/* USER CODE END Includes */
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+int main(void){
+    HAL_Init();
 
-/* USER CODE END PTD */
+    
+    GPIO_InitTypeDef GPIOA_InitStructure = {};
+    __HAL_RCC_GPIOB_CLK_ENABLE();   
+    __HAL_RCC_GPIOA_CLK_ENABLE();
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
+    GPIOB->MODER &= (~(0xFFFF << 2));
+    GPIOB->MODER |= (0x5555 << 2);
+    
+    GPIOA_InitStructure.Pin = GPIO_PIN_15;
+    GPIOA_InitStructure.Mode = GPIO_MODE_INPUT;
+    GPIOA_InitStructure.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIOA_InitStructure);
+    
+    GPIOA_InitStructure.Pin = GPIO_PIN_14;
+    HAL_GPIO_Init(GPIOA, &GPIOA_InitStructure);
 
-/* USER CODE END PD */
+    GPIOA_InitStructure.Pin = GPIO_PIN_5;
+    HAL_GPIO_Init(GPIOA, &GPIOA_InitStructure);
+    
+    uint32_t Pt; 
+    uint32_t Input = 0;
+    
+    Pt = goN;
+    
+while(1) {
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
+uint8_t north = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_15);
+uint8_t east  = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_14);
+uint8_t ped   = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5);
 
-/* USER CODE END PM */
+if(!north && !east && !ped)          Input = 0; 
+else if(!north && !east && ped)      Input = 1; 
+else if(!north && east && !ped)      Input = 2; 
+else if(!north && east && ped)       Input = 3; 
+else if(north && !east && !ped)      Input = 4; 
+else if(north && !east && ped)       Input = 5; 
+else if(north && east && !ped)       Input = 6; 
+else if(north && east && ped)        Input = 7; 
 
-/* Private variables ---------------------------------------------------------*/
 
-/* USER CODE BEGIN PV */
+   
+    Pt = FSM[Pt].Next[Input];
 
-/* USER CODE END PV */
+    
+    int32_t bMask = 0x1FE;  // PB1-PB8
+    uint32_t oValue = (FSM[Pt].Out << 1) & bMask;
+    GPIOB->ODR = (GPIOB->ODR & ~bMask) | oValue;
 
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+    // Wait for the specified state time
+    HAL_Delay(FSM[Pt].Time);
 }
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
-  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
 }
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, LD4_Pin|LD3_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : LD4_Pin LD3_Pin */
-  GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-#ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
